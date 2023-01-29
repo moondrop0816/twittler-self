d305a10657d585abf2f0fe028d9b12a08dd48b31
moondrop0816 <moondrop0816@gmail.com>
2023-01-29 17:14:52 +0900

    트윗 클릭시 수정모드 구현

--- src/Components/Tweet.js
+++ src/Components/Tweet.js
@@ -1,6 +1,13 @@
+import { useState, useRef } from "react";
+
 const Tweet = ({ tweet }) => {
   const parseDate = (createdAt) =>
     new Date(createdAt).toLocaleDateString("ko-KR");
+  const [isEdit, setIsEdit] = useState(false); // 트윗 수정상태
+  const textRef = useRef();
+  const onEdit = () => {
+    setIsEdit(!isEdit);
+  };
   return (
     <li>
       <div className="tweet-avatar">
@@ -11,7 +18,18 @@ const Tweet = ({ tweet }) => {
           <span className="tweet-username">{tweet.username}</span>
           <span className="tweet-date">{parseDate(tweet.createdAt)}</span>
         </div>
-        <div className="tweet-msg">{tweet.content}</div>
+        {isEdit ? (
+          <textarea
+            className="tweet-msg-edit"
+            value={tweet.content}
+            style={{ height: textRef.current.offsetHeight + "px" }}
+            onClick={onEdit}
+          ></textarea>
+        ) : (
+          <div className="tweet-msg" ref={textRef} onClick={onEdit}>
+            {tweet.content}
+          </div>
+        )}
       </div>
     </li>
   );
